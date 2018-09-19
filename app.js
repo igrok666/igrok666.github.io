4164eb8f336ca668ffa7cc3f7f41d705afacb70f
igrok666 <programmer.urgzk@gmail.com>
2018-09-20 00:28:17 +0500

    first commit

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
 $(document).ready(function () {
-        $('.numb').number_plugin({
-            negative: false,
-            step: 300,
-            // animate: true,
-            // delay: 10,
-        });
+        $('.numb').number_plugin();
     });
