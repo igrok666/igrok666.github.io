aa83b5e5ca93092362f393f6c1d391a56385931a
igrok666 <programmer.urgzk@gmail.com>
2018-09-24 21:28:26 +0500

    Changes

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 $(document).ready(function () {
-        $('.numb').number_plugin();
+        $('.numb').number_plugin({
+            width: '65px',
+            height: '35px',
+            negative: true,
+            step: 20,
+            animate: true,
+            delay: 100,
+        });
     });
